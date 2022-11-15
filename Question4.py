598cb61820ffb81eada0a1eb0aaa85fb05050abc
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-15 23:46:20 +0530

    Add files via upload

---
+++ Question4.py
@@ -0,0 +1,16 @@
+### 4. Write a Python program to swap two variables?
+
+var1=123;
+var2=111;
+
+print("Before Swap var1 is ",var1,"and var2 is ",var2)
+
+temp=var1;
+var1=var2;
+var2=temp;
+
+print("After swaping number var1 is ",var1,"and var2 is ",var2);
+
+#Output 
+#Before Swap var1 is  123 and var2 is  111
+#After swaping number var1 is  111 and var2 is  123
