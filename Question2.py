7ab980841f8f39352306537b632820ab16ed41c7
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-15 23:24:43 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,3 +1,5 @@
+#### 2. Write a Python program to do arithmetical operations addition and division.?
+
 var1 = 10;
 var2 =2;
 #Addition
@@ -7,3 +9,7 @@ print("The addition is ",add)
 #Division
 div=var1/var2
 print("The dividion is ",div)
+
+#Output
+#The addition is  12
+#The dividion is  5.0
