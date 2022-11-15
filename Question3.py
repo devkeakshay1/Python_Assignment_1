68aea4cedd37bc7d7cad7e5bd6e8915deed62e6d
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-15 23:30:18 +0530

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,12 @@
+### 3. Write a Python program to find the area of a triangle?
+
+height=100;
+base=25;
+
+#Calculate Area Of trangle using formula
+area=height*base/2;
+
+print("Area of a triangle is ",area);
+
+#Output
+#area of a triangle is  1250.0
